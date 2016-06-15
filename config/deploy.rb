915fc042d45abf70a15b172e059f26abdfb6f1c9
alexey <alexeymazurik@gmail.com>
2016-06-16 01:41:51 +0300

    Added queue name

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,6 +22,8 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :sidekiq_queue, '-q recommendable'
+
 
 ## Defaults:
 # set :scm,           :git
